ecb4f9033cae4b9fb93ea3039cdf542c1d2694c6
jackdperfecto <jackd@perfectomobile.com>
2018-01-02 10:06:23 -0500

    initial commit

---
+++ app/src/androidTest/java/com/example/jackd/testapp/MainActivityTest.java
@@ -0,0 +1,61 @@
+package com.example.jackd.testapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.SmallTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
+
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+
+        onView(withId(R.id.fab)).perform(click());
+        onView(allOf(withText("Open some thing"))).check(matches(isDisplayed()));
+        onView(withText("Hello World!")).perform(doubleClick());
+
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText("Settings")).check(matches(withText("Settings")));
+
+        //onView(withText("Hello World!")).perform(doubleClick());
+
+        onView(withId(R.id.toolbar)).check(matches(withText("testapp")));
+
+    }
+
+
+}
